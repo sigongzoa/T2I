c61db9b1cd00e15ba301188f762c9fd738709b2e
sigongzoa <dlarhkdgml12@naver.com>
2018-12-10 08:48:09 +0900

    123

---
+++ recommend/reject.py
@@ -0,0 +1,16 @@
+### 사용자가 다시돌리기했을때 실행될 모듈 ###
+
+
+# 임의로 만든 데이터
+
+user_data = {'ID':1234,'PW':1234,'age':31,'gender':0,'category':[10,50,10,10,10],'price':[50,30,0,0,0],'distance':[15,20,15,10,10]}
+
+result = {'name': '순이네칼국수', 'distance': 181, 'score': 72.0, 'category': 2, 'price': 12000, 'age10': 30, 'age20': 40, 'age30': 40, 'age40': 80, 'age50': 60, 'male': 30, 'female': 70, 'rating': 5.0, 'partial_price': 3, 'partial_distance': 3, 'hitrate': 29.674796747967477}
+
+
+# 추천된 식당의 속성에 대한 사용자의 선호도를 깎음
+user_data['category'][result['category']-2] = user_data['category'][result['category']-2] - 5
+user_data['price'][result['partial_price']-2] = user_data['price'][result['partial_price']-2] - 5
+user_data['distance'][result['partial_distance']-2] = user_data['distance'][result['partial_distance']-2] - 5
+
+print(user_data)
