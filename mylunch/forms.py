b253d5fbf8f32ae3bae93953076ae18370cce49f
Junyeob <juny116@gmail.com>
2018-12-12 06:09:08 +0900

    Merge branch 'master' of https://github.com/sigongzoa/T2I
    # Conflicts:
    #	crawl/WEB_crawling.py

--- mylunch/forms.py
+++ mylunch/forms.py
@@ -1,14 +1,9 @@
 from django import forms
-from decimal import Decimal
-from capstone import settings
 from mylunch import choices as ch
-from mylunch.models import *
-import datetime
 
 class Filter_form(forms.Form):
     type = forms.DecimalField(label="type", max_digits=1, decimal_places=0,widget=forms.Select(choices=ch.TYPE, attrs={'style': 'width:200px'}),required=False,initial=0)
     price = forms.DecimalField(label="price", max_digits=1, decimal_places=0, widget=forms.Select(choices=ch.PRICE, attrs={'style': 'width:200px'}), required=False, initial=0)
     exp = forms.DecimalField(label="exp", max_digits=1, decimal_places=0, widget=forms.Select(choices=ch.EXP, attrs={'style': 'width:200px'}), required=False, initial=0)
     distance = forms.DecimalField(label="distance", max_digits=1, decimal_places=0, widget=forms.Select(choices=ch.DISTANCE, attrs={'style': 'width:200px'}), required=False, initial=0)
-
 
